c52842f8713e6fc193fb08a5259ce6a05495c1c7
Jethet <henriette.hettinga@gmail.com>
2020-10-13 18:40:38 +0200

    Add starter code for project

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function setup() {
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
+}
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+}
+
+window.onload = setup;
